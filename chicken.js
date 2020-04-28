697ec4d0f2ac34587ea664995f18199e94f1db53
jcwangg <35509145+jcwangg@users.noreply.github.com>
2020-04-28 11:18:42 -0700

    Add files via upload

---
+++ chicken.js
@@ -0,0 +1,74 @@
+
+"use strict";
+
+(function(){
+  /**
+  This method calls the startFunction to display the home page and calls the start
+  function on click of the "home" button.
+  */
+  function pageLoad(){
+    document.getElementById("menuPage").style.display = "none";
+    document.getElementById("storyPage").style.display = "none";
+    document.getElementById("menuButton1").onclick = displayMenu;
+    document.getElementById("menuButton2").onclick = displayMenu;
+    document.getElementById("homeButton1").onclick = displayHome;
+    document.getElementById("homeButton2").onclick = displayHome;
+    document.getElementById("logo").onclick = displayHome;
+    document.getElementById("storyButton1").onclick = displayStory;
+    document.getElementById("storyButton2").onclick = displayStory;
+    var el = document.getElementsByClassName("button");
+    for (var i in el){
+        el[i].onmouseover = function(){
+                this.style.color = "Red";
+        };
+        el[i].onmouseout  = function(){
+                this.style.color = "White";
+        };
+      }
+    //alert("in");
+  //  var buttons = document.getElementsByClassName("button");
+  //  for (var i = 0; i<buttons.length;i++){
+    //  buttons[i].onclick = alert("why");
+    //onmouseover = changeRed;
+  //  displayHome();
+  }
+
+function displayStory(){
+  document.getElementById("menuPage").style.display = "none";
+  document.getElementById("main").style.display = "none";
+  document.getElementById("main2").style.display = "none";
+  document.getElementById("storyPage").style.display = "block";
+}
+
+function displayHome(){
+  document.getElementById("menuPage").style.display = "none";
+  document.getElementById("main").style.display = "block";
+  document.getElementById("main2").style.display = "flex";
+  document.getElementById("storyPage").style.display = "none";
+
+}
+
+function displayMenu(){
+  document.getElementById("menuPage").style.display = "block";
+  document.getElementById("main").style.display = "none";
+  document.getElementById("main2").style.display = "none";
+  document.getElementById("storyPage").style.display = "none";
+  //document.getElementById("menuPage").style.display = "none";
+
+}
+  function changeRed(){
+    alert("asd");
+    this.style.color = "Red";
+  }
+
+  function changeWhite(){
+    this.style.color = "White";
+
+  }
+
+  function changeBlack(){
+    this.style.color = "Black";
+
+  }
+  window.addEventListener("load", pageLoad);
+})();
